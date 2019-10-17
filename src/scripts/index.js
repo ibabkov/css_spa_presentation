8dbdc27c65ef90530665b3fed16f98b82c81081c
Иван Бабков <i.babkov@cian.ru>
2019-10-17 21:30:57 +0300

    Fix js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,28 +5,40 @@
     const LEFT = 37;
     const RIGHT = 39;
 
+    const isNavKey = (event) => {
+      return event.key !== NEXT
+          && event.key !== PREV
+          && event.keyCode !== SPACE
+          && event.keyCode !== LEFT
+          && event.keyCode !== RIGHT;
+    };
+    const getWayMod = (event) => {
+      return event.key === NEXT
+      || event.keyCode === SPACE
+      || event.keyCode === RIGHT
+        ? 1
+        : -1;
+    };
+    const getNextPageHash = (event) => {
+      const currentHash = window.location.hash;
+      const currentPageMatched = currentHash.match(/\d/);
+      const currentPage = currentPageMatched ? Number(currentPageMatched[0]) : 1;
+      const mod = getWayMod(event);
+      return `slide-${currentPage + mod}`;
+    };
+
     document.addEventListener('keydown', (event) => {
-        if(
-            event.key !== NEXT
-            && event.key !== PREV
-            && event.keyCode !== SPACE
-            && event.keyCode !== LEFT
-            && event.keyCode !== RIGHT
-        ) {
+        // if this is not navigation key - do nothing
+        if(isNavKey(event)) {
             return;
         }
-        event.preventDefault();
-        const currentHash = window.location.hash;
-        let currentPageMatched = currentHash.match(/\d/);
-        const currentPage = currentPageMatched ? Number(currentPageMatched[0]) : 1;
-        const mod =
-            event.key === NEXT
-            || event.keyCode === SPACE
-            || event.keyCode === RIGHT
-                ? 1
-                : -1;
-        const nextPageHash = `slide-${currentPage + mod}`;
 
+        // prevent scrolling
+        event.preventDefault();
+
+        const nextPageHash = getNextPageHash(event);
+
+        // if there's no necessary selector  - do nothing
         if (!document.querySelector(`[href="#${nextPageHash}"]`)) {
             return;
         }
