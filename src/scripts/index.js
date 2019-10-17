188b20aeceb61f176de8e972b73b515fbb395728
Иван Бабков <i.babkov@cian.ru>
2019-10-17 21:20:59 +0300

    Init js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,18 +1,31 @@
 (() => {
     const NEXT = 'PageUp';
     const PREV = 'PageDown';
+    const SPACE = 32;
+    const LEFT = 37;
+    const RIGHT = 39;
 
     document.addEventListener('keydown', (event) => {
-        if(event.key !== NEXT && event.key !== PREV) {
+        if(
+            event.key !== NEXT
+            && event.key !== PREV
+            && event.keyCode !== SPACE
+            && event.keyCode !== LEFT
+            && event.keyCode !== RIGHT
+        ) {
             return;
         }
         event.preventDefault();
-
         const currentHash = window.location.hash;
         let currentPageMatched = currentHash.match(/\d/);
         const currentPage = currentPageMatched ? Number(currentPageMatched[0]) : 1;
-        const mod = event.key === NEXT ? 1 : -1;
-        const nextPageHash = `page-${currentPage + mod}`;
+        const mod =
+            event.key === NEXT
+            || event.keyCode === SPACE
+            || event.keyCode === RIGHT
+                ? 1
+                : -1;
+        const nextPageHash = `slide-${currentPage + mod}`;
 
         if (!document.querySelector(`[href="#${nextPageHash}"]`)) {
             return;
